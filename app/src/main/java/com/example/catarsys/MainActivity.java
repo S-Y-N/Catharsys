2a8d5f6ae1afb5f942c49602299bcbf79d754ba6
S-Y-N <duostal@gmail.com>
2023-07-04 12:39:16 +0300

    add logout and save app state

--- app/src/main/java/com/example/catarsys/MainActivity.java
+++ app/src/main/java/com/example/catarsys/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.catarsys;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.catarsys.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding _binding;
+    FirebaseAuth _auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        _binding =ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(_binding.getRoot());
+
+        _auth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.settings){
+            Toast.makeText(this,"Setting",Toast.LENGTH_SHORT).show();
+        }else if(id == R.id.groupChat){
+            Toast.makeText(this,"Group Chat",Toast.LENGTH_SHORT).show();
+        }else if(id == R.id.logout){
+            _auth.signOut();
+            Intent intent = new Intent(MainActivity.this,SignInActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
